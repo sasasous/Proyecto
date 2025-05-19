d60b916035484005d113da2ca951842c0666ad6b
sasasous <t4perezduartesofia@gmail.com>
2025-05-19 20:02:09 -0300

    controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,30 +3,32 @@ const indexController = {
         return res.render('index.ejs');
     },
     show: function(req, res) {
-        return res.render('id.ejs');
+        return res.render('datailProduct.ejs');
     }
 };
 
 const db = require('../database/models');
 
-module.exports = {
-    home: (req, res) {
-        db.product.findAll()
-            .then(products => {
-                res.render('index', { products });
-            })
-            .catch(error => {
-                console.log(error);
-                return res.status(500).send('Error');
-            });
-    },
+    module.exports = {
+        home: (req, res) => {
+            db.Product.findAll()
+                .then((products) => {
+                    return res.render('index', { products });
+                })
+                .catch((error) => {
+                    console.log(error);
+                    return res.status(500).send('Error');
+                });
+        },
 
-    detailProduct: (req, res) => {
-        db.product.findByPk(req.params.id)
-            .then(product => {
-                res.render('detailProduct', { product });
-            })
-    }
-};
-
-module.exports = indexController;
+        detailProduct: (req, res) => {
+            db.Product.findByPk(req.params.id)
+                .then((product) => {
+                    return res.render('detailProduct', { product });
+                })
+                .catch((error) => {
+                    console.log(error);
+                    return res.status(500).send('Error');
+                });
+        }
+}
