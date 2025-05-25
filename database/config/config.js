ffd9a3d1e3d0a6629cddf5714f6a0b37e52557f9
SOuS <t4perezduartesofia@gmail.com>
2025-05-25 04:25:19 -0300

    pass

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "sofia752.",
     "database": "sous_market",
     "host": "127.0.0.1",
     "dialect": "mysql"
